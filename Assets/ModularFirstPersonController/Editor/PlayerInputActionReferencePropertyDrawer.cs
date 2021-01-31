672d9cbfcce471c914e6d5598f8d2351c4030745
Nick Gravelyn <nick@nickgravelyn.com>
2021-01-31 11:11:52 -0800

    Better type for referencing input actions, including inspector UI
    New PlayerInputActionReference gives a much nicer storage of input
    actions by storing their IDs rather than their names. This makes it safe
    to rename actions without breaking references.
    
    Additionally this adds a property drawer so those fields can be shown
    with a nice popup selector populated with the names of actions in the
    PlayerInput attached to the same object.

---
+++ Assets/ModularFirstPersonController/Editor/PlayerInputActionReferencePropertyDrawer.cs
@@ -0,0 +1,50 @@
+﻿#if ENABLE_INPUT_SYSTEM
+
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Linq;
+using System;
+
+namespace ModularFirstPerson
+{
+    [CustomPropertyDrawer(typeof(PlayerInputActionReference))]
+    public class PlayerInputActionReferencePropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            var obj = property.serializedObject.targetObject as MonoBehaviour;
+            var playerInput = obj.GetComponentInParent<PlayerInput>();
+            var actions = playerInput.actions;
+
+            var options = actions.actionMaps.SelectMany(map => 
+                map.actions.Select(action => new { name = $"{map.name}/{action.name}", action.id })
+            ).ToList();
+
+            var optionNames = options.Select(o => o.name).ToArray();
+
+            var guidAsStringProp = property.FindPropertyRelative("guidAsString");
+
+            int currentIndex = -1;
+            if (Guid.TryParse(guidAsStringProp.stringValue, out var guid))
+            {
+                currentIndex = options.FindIndex(o => o.id == guid);
+            }
+
+            var newIndex = EditorGUI.Popup(position, currentIndex, optionNames);
+
+            if (newIndex != currentIndex)
+            {
+                guidAsStringProp.stringValue = options[newIndex].id.ToString();
+            }
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
+
+#endif // #if ENABLE_INPUT_SYSTEM
--- Assets/ModularFirstPersonController/Runtime/ActionBasedPlayerInputProvider.cs
+++ Assets/ModularFirstPersonController/Runtime/ActionBasedPlayerInputProvider.cs
@@ -1,25 +1,26 @@
 ﻿#if ENABLE_INPUT_SYSTEM
 
+using System;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace ModularFirstPerson
 {
-    [RequireComponent(typeof(PlayerInput))]
     public sealed class ActionBasedPlayerInputProvider : PlayerInputProvider
     {
-        private InputAction _lookAction;
-        private InputAction _moveAction;
+        private InputAction _lookActionRef;
+        private InputAction _moveActionRef;
 
         private Vector2 _look;
         private Vector2 _move;
 
         // TODO: In-editor validation of these values and/or some kind of nice action picker UI
-        [SerializeField, Tooltip("The name of an action that provides a Vector2 for turning and looking up/down.")]
-        private string _lookActionName = "Look";
+        [SerializeField, Tooltip("An action that provides a Vector2 for turning and looking up/down.")]
+        private PlayerInputActionReference _lookAction;
 
-        [SerializeField, Tooltip("The name of an action that provides a Vector2 for moving.")]
-        private string _moveActionName = "Move";
+        [SerializeField, Tooltip("An action that provides a Vector2 for moving.")]
+        private PlayerInputActionReference _moveAction;
 
         public override float lookHorizontal => _look.x;
         public override float lookVertical => _look.y;
@@ -27,24 +28,37 @@ namespace ModularFirstPerson
 
         private void OnEnable()
         {
-            var playerInput = GetComponent<PlayerInput>();
+            _look = Vector2.zero;
+            _move = Vector2.zero;
 
-            _lookAction = playerInput.actions.FindAction(_lookActionName, throwIfNotFound: true);
-            _lookAction.performed += OnLookPerformed;
-            _lookAction.canceled += OnLookCanceled;
+            var playerInput = GetComponentInParent<PlayerInput>();
 
-            _moveAction = playerInput.actions.FindAction(_moveActionName, throwIfNotFound: true);
-            _moveAction.performed += OnMovePerformed;
-            _moveAction.canceled += OnMoveCanceled;
+            if (_lookAction.TryGetInputAction(playerInput, out _lookActionRef))
+            {
+                _lookActionRef.performed += OnLookPerformed;
+                _lookActionRef.canceled += OnLookCanceled;
+            }
+
+            if (_moveAction.TryGetInputAction(playerInput, out _moveActionRef))
+            {
+                _moveActionRef.performed += OnMovePerformed;
+                _moveActionRef.canceled += OnMoveCanceled;
+            }
         }
 
         private void OnDisable()
         {
-            _lookAction.performed -= OnLookPerformed;
-            _lookAction.canceled -= OnLookCanceled;
+            if (_lookActionRef != null)
+            {
+                _lookActionRef.performed -= OnLookPerformed;
+                _lookActionRef.canceled -= OnLookCanceled;
+            }
 
-            _moveAction.performed -= OnMovePerformed;
-            _moveAction.canceled -= OnMoveCanceled;
+            if (_moveActionRef != null)
+            {
+                _moveActionRef.performed -= OnMovePerformed;
+                _moveActionRef.canceled -= OnMoveCanceled;
+            }
         }
 
         private void OnLookPerformed(InputAction.CallbackContext ctx)
