02e80929fe65f3b187524020c062ad8f08525e49
Nick Gravelyn <nick@nickgravelyn.com>
2021-01-31 07:47:04 -0800

    Cleaning up PlayerLookUpDown and moving it to cam root
    This lets the component be on the game object it actually controls which
    I like. It does require a search through parents for the input provider
    but that's a one-time startup cost.

--- Assets/ModularFirstPersonController/Runtime/PlayerLookUpDown.cs
+++ Assets/ModularFirstPersonController/Runtime/PlayerLookUpDown.cs
@@ -5,13 +5,10 @@ namespace ModularFirstPerson
 {
     public sealed class PlayerLookUpDown : MonoBehaviour
     {
+        private Transform _transform;
+        private PlayerInputProvider _input;
+
         private float _pitch = 0f;
-
-        [SerializeField, Tooltip("The component that provides user input to process.")]
-        private PlayerInputProvider _input = default;
-
-        [SerializeField, Tooltip("The transform that will be rotated on the X axis based on player input.")]
-        private Transform _targetTransform = default;
 
         [Tooltip("The minimum pitch value allowed, specified in degrees.")]
         public float minPitch = -85f;
@@ -19,23 +16,16 @@ namespace ModularFirstPerson
         [Tooltip("The maximum pitch value allowed, specified in degrees.")]
         public float maxPitch = 85f;
 
+        private void OnEnable()
+        {
+            _transform = transform;
+            _input = GetComponentInParent<PlayerInputProvider>();
+        }
+
         private void Update()
         {
             _pitch = Clamp(_pitch + _input.lookVertical, minPitch, maxPitch);
-            _targetTransform.localEulerAngles = new Vector3(_pitch, 0, 0);
-        }
-
-        private void OnValidate()
-        {
-            if (_input == null)
-            {
-                _input = GetComponent<PlayerInputProvider>();
-            }
-
-            if (!_targetTransform)
-            {
-                _targetTransform = transform;
-            }
+            _transform.localEulerAngles = new Vector3(_pitch, 0, 0);
         }
     }
 }
