15af244aa13ccd7922495bd5337751555c078cce
Nick Gravelyn <nick@nickgravelyn.com>
2021-01-30 20:09:24 -0800

    Splitting Look component into PlayerTurn and PlayerLookUpDown

--- Scripts/Runtime/PlayerLookUpDown.cs
+++ Scripts/Runtime/PlayerLookUpDown.cs
@@ -3,15 +3,14 @@ using static UnityEngine.Mathf;
 
 namespace ModularFirstPerson
 {
-    public sealed class Look : MonoBehaviour
+    public sealed class PlayerLookUpDown : MonoBehaviour
     {
         private float _pitch = 0f;
-        private float _yaw = 0f;
 
         [SerializeField, Tooltip("The component that provides user input to process.")]
         private PlayerInputProvider _input = default;
 
-        [SerializeField, Tooltip("The transform that will be rotated based on the yaw/pitch values.")]
+        [SerializeField, Tooltip("The transform that will be rotated on the X axis based on player input.")]
         private Transform _targetTransform = default;
 
         [Tooltip("The minimum pitch value allowed, specified in degrees.")]
@@ -23,9 +22,7 @@ namespace ModularFirstPerson
         private void Update()
         {
             _pitch = Clamp(_pitch + _input.lookVertical, minPitch, maxPitch);
-            _yaw = Repeat(_yaw + _input.lookHorizontal, 360f);
-
-            _targetTransform.localEulerAngles = new Vector3(_pitch, _yaw, 0);
+            _targetTransform.localEulerAngles = new Vector3(_pitch, 0, 0);
         }
 
         private void OnValidate()
---
+++ Scripts/Runtime/PlayerTurn.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using static UnityEngine.Mathf;
+
+namespace ModularFirstPerson
+{
+    public sealed class PlayerTurn : MonoBehaviour
+    {
+        [SerializeField, Tooltip("The component that provides user input to process.")]
+        private PlayerInputProvider _input = default;
+
+        [SerializeField, Tooltip("The transform that will be turned around the Y axis based on the player input.")]
+        private Transform _targetTransform = default;
+
+        private void Update()
+        {
+            var yaw = _targetTransform.localEulerAngles.y;
+            yaw = Repeat(yaw + _input.lookHorizontal, 360f);
+            _targetTransform.localEulerAngles = new Vector3(0, yaw, 0);
+        }
+
+        private void OnValidate()
+        {
+            if (_input == null)
+            {
+                _input = GetComponent<PlayerInputProvider>();
+            }
+
+            if (!_targetTransform)
+            {
+                _targetTransform = transform;
+            }
+        }
+    }
+}
