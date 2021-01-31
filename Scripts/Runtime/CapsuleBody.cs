5ac6e3d23d62a99b718388b53787debf75061707
Nick Gravelyn <nick@nickgravelyn.com>
2021-01-30 20:17:39 -0800

    Extracting "player" logic out of CapsuleBody
    This restores CapsuleBody to being a "primitive" component in that it
    provides some management of, and a wrapper around, a RigidBody and a
    CapsuleCollider. It no longer contains any notion of player input,
    gravity, velocity etc.
    
    Instead we now have PlayerController which is one way to drive the
    CapsuleCollider. My goal for PlayerController (which will likely get
    renamed) is that it provides a very reasonable default controller but
    is ultimately an optional piece of the system. Games with specific
    requirements may want to implement their own version. Or perhaps I split
    this up into something more configurable. Either way I expect this area
    to have a lot of change since it gets into pretty game specific logic
    easily.

--- Scripts/Runtime/CapsuleBody.cs
+++ Scripts/Runtime/CapsuleBody.cs
@@ -4,29 +4,12 @@ namespace ModularFirstPerson
 {
     [RequireComponent(typeof(Rigidbody))]
     [RequireComponent(typeof(CapsuleCollider))]
-    public class CapsuleBody : MonoBehaviour
+    public sealed class CapsuleBody : MonoBehaviour
     {
         private readonly Collider[] _overlapColliders = new Collider[16];
 
-        private bool _grounded;
-        private RaycastHit _lastGroundHit;
-
-        private float _verticalVelocity;
-
-        // Control velocity based on movement input and the ground normal
-        private Vector3 _controlVelocity;
-
-        // Final computed velocity carried between frames for acceleration
-        private Vector3 _velocity;
-
-        [SerializeField, HideInInspector]
-        private Rigidbody _body = default;
-
-        [SerializeField, HideInInspector]
-        private CapsuleCollider _collider = default;
-
-        [SerializeField, Tooltip("The component responsible for providing input to the motor.")]
-        private PlayerInputProvider _input;
+        private Rigidbody _body;
+        private CapsuleCollider _collider;
 
         [SerializeField, Tooltip("The total height of the character. The capsule height is this value minus the step height.")]
         private float _height = 1.7f;
@@ -42,19 +25,6 @@ namespace ModularFirstPerson
 
         [Tooltip("An extra value used when sweeping the capsule through the world to improve collision detection.")]
         public float skinThickness = 0.1f;
-
-        [SerializeField]
-        public float acceleration = 2f;
-
-        [SerializeField]
-        public float airDrag = 0.2f;
-
-        [SerializeField]
-        public float airControl = 20f;
-
-        public PlayerSpeed speed = new PlayerSpeed(2f, 1f, 0.95f);
-
-        public Transform cameraForward;
 
         public Bounds bounds =>
             new Bounds(
@@ -123,76 +93,6 @@ namespace ModularFirstPerson
             _body.detectCollisions = false;
         }
 
-        private void FixedUpdate()
-        {
-            CheckForGround();
-
-            var moveInput = _input.moveInput;
-            ApplyUserInputMovement(in moveInput);
-
-            _velocity = _controlVelocity + new Vector3(0, _verticalVelocity, 0);
-            MoveWithVelocity(ref _velocity);
-        }
-
-        private void ApplyUserInputMovement(in Vector2 moveInput)
-        {
-            var movementRotation = Quaternion.Euler(0, cameraForward.eulerAngles.y, 0);
-            if (_grounded)
-            {
-                movementRotation = Quaternion.FromToRotation(Vector3.up, _lastGroundHit.normal) * movementRotation;
-            }
-
-            var moveVelocity = movementRotation * new Vector3(moveInput.x, 0, moveInput.y);
-            var targetSpeed = Mathf.Lerp(_controlVelocity.magnitude, speed.TargetSpeed(moveInput), acceleration * Time.deltaTime);
-            moveVelocity *= targetSpeed;
-
-            if (_grounded)
-            {
-                // 100% control on ground
-                _controlVelocity = moveVelocity;
-            }
-            else
-            {
-                if (moveVelocity.sqrMagnitude > 0)
-                {
-                    moveVelocity = Vector3.ProjectOnPlane(moveVelocity, Vector3.up);
-                    _controlVelocity = Vector3.Lerp(_controlVelocity, moveVelocity, airControl * Time.deltaTime);
-                }
-
-                ApplyAirDrag();
-            }
-        }
-
-        private void ApplyAirDrag()
-        {
-            _controlVelocity *= (1f / (1f + (airDrag * Time.fixedDeltaTime)));
-        }
-
-        private void CheckForGround()
-        {
-            var hitGround = CheckForGround(_grounded, out _lastGroundHit, out var verticalMovementApplied);
-
-            // Only grounded if the body detected ground AND we're not moving upwards
-            var groundedNow = hitGround && _verticalVelocity <= 0;
-            var wasGrounded = _grounded;
-
-            _grounded = groundedNow;
-
-            if (!wasGrounded && groundedNow)
-            {
-                // TODO: OnBeginGrounded event
-            }
-
-            if (groundedNow)
-            {
-                _verticalVelocity = 0;
-
-                // Reproject our control velocity onto the ground plane without losing magnitude
-                var groundNormal = _lastGroundHit.normal;
-                _controlVelocity = (_controlVelocity - groundNormal * Vector3.Dot(_controlVelocity, groundNormal)).normalized * _controlVelocity.magnitude;
-            }
-        }
-
         public void MoveWithVelocity(ref Vector3 velocity)
         {
             // NOTE: Capping iterations here to avoid any chance of an infinite
@@ -204,8 +104,8 @@ namespace ModularFirstPerson
             var movement = velocity * Time.deltaTime;
 
             for (
-                int iteration = 0; 
-                iteration < MaxIterations && !Mathf.Approximately(movement.sqrMagnitude, 0); 
+                int iteration = 0;
+                iteration < MaxIterations && !Mathf.Approximately(movement.sqrMagnitude, 0);
                 iteration++
             )
             {
@@ -392,12 +292,6 @@ namespace ModularFirstPerson
         {
             _body = GetComponent<Rigidbody>();
             _collider = GetComponent<CapsuleCollider>();
-
-            if (!_input)
-            {
-                _input = GetComponent<PlayerInputProvider>();
-            }
-
             ResizeCollider();
         }
     }
---
+++ Scripts/Runtime/PlayerController.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+
+namespace ModularFirstPerson
+{
+    public sealed class PlayerController : MonoBehaviour
+    {
+        private bool _grounded;
+        private RaycastHit _lastGroundHit;
+
+        private float _verticalVelocity;
+
+        // Control velocity based on movement input and the ground normal
+        private Vector3 _controlVelocity;
+
+        // Final computed velocity carried between frames for acceleration
+        private Vector3 _velocity;
+
+        [SerializeField, Tooltip("The component responsible for providing input to the motor.")]
+        private PlayerInputProvider _input;
+
+        [SerializeField]
+        private CapsuleBody _body;
+
+        [SerializeField]
+        public float acceleration = 2f;
+
+        [SerializeField]
+        public float airDrag = 0.2f;
+
+        [SerializeField]
+        public float airControl = 20f;
+
+        public PlayerSpeed speed = new PlayerSpeed(2f, 1f, 0.95f);
+
+        public Transform cameraForward;
+
+        private void OnValidate()
+        {
+            if (!_input)
+            {
+                _input = GetComponent<PlayerInputProvider>();
+            }
+
+            if (!_body)
+            {
+                _body = GetComponent<CapsuleBody>();
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            CheckForGround();
+
+            var moveInput = _input.moveInput;
+            ApplyUserInputMovement(in moveInput);
+
+            _velocity = _controlVelocity + new Vector3(0, _verticalVelocity, 0);
+            _body.MoveWithVelocity(ref _velocity);
+        }
+
+        private void ApplyUserInputMovement(in Vector2 moveInput)
+        {
+            var movementRotation = Quaternion.Euler(0, cameraForward.eulerAngles.y, 0);
+            if (_grounded)
+            {
+                movementRotation = Quaternion.FromToRotation(Vector3.up, _lastGroundHit.normal) * movementRotation;
+            }
+
+            var moveVelocity = movementRotation * new Vector3(moveInput.x, 0, moveInput.y);
+            var targetSpeed = Mathf.Lerp(_controlVelocity.magnitude, speed.TargetSpeed(moveInput), acceleration * Time.deltaTime);
+            moveVelocity *= targetSpeed;
+
+            if (_grounded)
+            {
+                // 100% control on ground
+                _controlVelocity = moveVelocity;
+            }
+            else
+            {
+                if (moveVelocity.sqrMagnitude > 0)
+                {
+                    moveVelocity = Vector3.ProjectOnPlane(moveVelocity, Vector3.up);
+                    _controlVelocity = Vector3.Lerp(_controlVelocity, moveVelocity, airControl * Time.deltaTime);
+                }
+
+                ApplyAirDrag();
+            }
+        }
+
+        private void ApplyAirDrag()
+        {
+            _controlVelocity *= (1f / (1f + (airDrag * Time.fixedDeltaTime)));
+        }
+
+        private void CheckForGround()
+        {
+            var hitGround = _body.CheckForGround(_grounded, out _lastGroundHit, out var verticalMovementApplied);
+
+            // Only grounded if the body detected ground AND we're not moving upwards
+            var groundedNow = hitGround && _verticalVelocity <= 0;
+            var wasGrounded = _grounded;
+
+            _grounded = groundedNow;
+
+            if (!wasGrounded && groundedNow)
+            {
+                // TODO: OnBeginGrounded event
+            }
+
+            if (groundedNow)
+            {
+                _verticalVelocity = 0;
+
+                // Reproject our control velocity onto the ground plane without losing magnitude
+                var groundNormal = _lastGroundHit.normal;
+                _controlVelocity = (_controlVelocity - groundNormal * Vector3.Dot(_controlVelocity, groundNormal)).normalized * _controlVelocity.magnitude;
+            }
+        }
+
+    }
+}
