17fd9393434025d7c823a0f53b1cc53c1d1f4459
Nick Gravelyn <nick@nickgravelyn.com>
2021-02-09 18:33:01 -0800

    Implementing new property drawer for player abilities
    Even with SerializedObjects for abilities, we need to be able to easily
    reorder them because the abilities list is prioritized. This is the most
    basic reorderable list implementation, but I might add some additional
    features later to allow editing the abilities inline, one of the main
    benefits of the managed references previously used.

---
+++ Assets/FirstPersonController/Editor/PlayerAbilitiesPropertyDrawer.cs
@@ -0,0 +1,74 @@
+﻿using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace FirstPersonController
+{
+    [CustomPropertyDrawer(typeof(PlayerAbilities))]
+    public class PlayerAbilitiesPropertyDrawer : PropertyDrawer
+    {
+        private ReorderableList _list;
+
+        public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+        {
+            var listProperty = property.FindPropertyRelative("_abilities");
+            var list = GetList(listProperty);
+            list.elementHeight = GetPropertyHeight(property, label);
+            list.DoList(rect);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            var listProperty = property.FindPropertyRelative("_abilities");
+            return GetList(listProperty).GetHeight();
+        }
+
+        private ReorderableList GetList(SerializedProperty serializedProperty)
+        {
+            if (_list == null)
+            {
+                _list = new ReorderableList(
+                    serializedProperty.serializedObject,
+                    serializedProperty,
+                    true,
+                    true,
+                    true,
+                    true
+                );
+
+                _list.drawHeaderCallback += OnDrawHeader;
+                _list.drawElementCallback += OnDrawElement;
+                _list.elementHeightCallback += GetElementHeight;
+            }
+
+            return _list;
+        }
+
+        private void OnDrawHeader(Rect rect)
+        {
+            EditorGUI.LabelField(rect, "Abilities");
+        }
+
+        private void OnDrawElement(Rect rect, int index, bool isActive, bool isFocused)
+        {
+            if (index <= _list.serializedProperty.arraySize)
+            {
+                var elementProp = _list.serializedProperty.GetArrayElementAtIndex(index);
+                EditorGUI.PropertyField(rect, elementProp);
+            }
+        }
+
+        private float GetElementHeight(int index)
+        {
+            if (index <= _list.serializedProperty.arraySize)
+            {
+                var elementProp = _list.serializedProperty.GetArrayElementAtIndex(index);
+                return EditorGUI.GetPropertyHeight(elementProp);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
