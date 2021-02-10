4b7705f9c8939bca67805a03c4217c7785f790b6
Nick Gravelyn <nick@nickgravelyn.com>
2021-02-09 20:59:19 -0800

    More cleanup in the abilities property drawer
    Did a bit of cleanup and removed the labels from the elements because
    they're not super necessary. I also fixed up the element height to
    ensure we're leaving space between the property fields which looks a bit
    nicer.

--- Assets/FirstPersonController/Editor/PlayerAbilitiesPropertyDrawer.cs
+++ Assets/FirstPersonController/Editor/PlayerAbilitiesPropertyDrawer.cs
@@ -9,49 +9,49 @@ namespace FirstPersonController
     {
         private ReorderableList _list;
 
-        public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
-        {
+        public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label) =>
             GetList(property).DoList(rect);
-        }
 
-        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-        {
-            return GetList(property).GetHeight();
-        }
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) =>
+            GetList(property).GetHeight();
 
-        private ReorderableList GetList(SerializedProperty property)
+        private ReorderableList GetList(SerializedProperty property) =>
+            _list ?? (_list = CreateList(property));
+
+        private static ReorderableList CreateList(SerializedProperty property)
         {
-            if (_list == null)
-            {
-                var serializedProperty = property.FindPropertyRelative("_abilities");
-                _list = new ReorderableList(
-                    serializedProperty.serializedObject,
-                    serializedProperty,
-                    true,
-                    true,
-                    true,
-                    true
+            var abilitiesProp = property.FindPropertyRelative("_abilities");
+            var list = new ReorderableList(
+                abilitiesProp.serializedObject,
+                abilitiesProp,
+                true,
+                true,
+                true,
+                true
+            );
+
+            list.drawHeaderCallback += rect =>
+                EditorGUI.LabelField(rect, "Abilities");
+
+            var spacing = EditorGUIUtility.standardVerticalSpacing;
+            
+            list.drawElementCallback += (rect, index, active, focused) =>
+                EditorGUI.PropertyField(
+                    new Rect(
+                        rect.x,
+                        rect.y + spacing / 2f,
+                        rect.width,
+                        rect.height - spacing
+                    ),
+                    list.serializedProperty.GetArrayElementAtIndex(index),
+                    GUIContent.none
                 );
 
-                _list.drawHeaderCallback += OnDrawHeader;
-                _list.drawElementCallback += OnDrawElement;
-                _list.elementHeight = EditorGUIUtility.singleLineHeight;
-            }
+            list.elementHeightCallback += index =>
+                EditorGUI.GetPropertyHeight(list.serializedProperty.GetArrayElementAtIndex(index)) +
+                spacing;
 
-            return _list;
-        }
-
-        private void OnDrawHeader(Rect rect)
-        {
-            EditorGUI.LabelField(rect, "Abilities");
-        }
-
-        private void OnDrawElement(Rect rect, int index, bool isActive, bool isFocused)
-        {
-            EditorGUI.PropertyField(
-                rect, 
-                _list.serializedProperty.GetArrayElementAtIndex(index)
-            );
+            return list;
         }
     }
 }
